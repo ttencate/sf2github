4b103072ef798c657f12bcafe1ffc0e8da74afc7
codeguru <codeguru@users.sourceforge.net>
2013-11-15 22:37:12 -0700

    Print first milestone and first ticket

--- sf2ghJSON.py
+++ sf2ghJSON.py
@@ -5,5 +5,18 @@ from pprint import pprint
 json_data=open('tickets.json')
 
 data = json.load(json_data)
-pprint(data)
+
+for d in data.keys():
+    pprint(d)
+
+print("-----------------------------------------")
+print("MILESTONES")
+print("-----------------------------------------")
+print(data["milestones"][0])
+
+print("-----------------------------------------")
+print("TICKETS")
+print("-----------------------------------------")
+pprint(data["tickets"][0])
+
 json_data.close()
