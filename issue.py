5f5e8cecf0e7209a31ffa3085593bc21594308d0
Benedikt Heine <bebe@bebehei.de>
2015-03-29 20:01:00 +0200

    include patches to bug-reports

--- issue.py
+++ issue.py
@@ -3,6 +3,7 @@ import requests
 import re
 
 import milestone as milestones
+import urllib2
 
 
 userdict = {
@@ -131,6 +132,13 @@ def addAllComments(auth, issueURL, sfPosts):
         else:
             post += 'Commented by '
         post += sfPost['author'] + ' on ' + timestamp + "**\n" + sfPost['text']
+        
+        for attach in sfPost['attachments']:
+            print("   Adding attached file: " + attach['url'])
+            post += "\nAttached file" + attach['url'].split('/')[-1] + ":\n\n"
+            for line in urllib2.urlopen(attach['url']):
+                post += '    ' + line
+
         (statusCode, message) = addComment(auth, issueURL, post)
         if statusCode == 201:
             successes += 1
