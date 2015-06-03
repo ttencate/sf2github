42d208144b16fd16490839fd0bbd89337dc917c2
Jani Patokallio <jpatokal@iki.fi>
2015-06-03 23:26:53 +1000

    Update issue.py

--- issue.py
+++ issue.py
@@ -142,8 +142,8 @@ def addAllComments(auth, issueURL, sfPosts):
                 for line in urllib2.urlopen(attach['url']):
                     post += '    ' + line
             except Exception as e:
-                print("   !!! Could not add attachment: " + e.reason)
-                post += "The file could not get attached: " + e.reason + "\n"
+                print("   !!! Could not add attachment: " + e)
+                post += "The file could not be attached: " + e + "\n"
                 failures += 1
 
         (statusCode, message) = addComment(auth, issueURL, post)
